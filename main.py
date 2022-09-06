39fffab08e59919405ad8fec7d44e79bd0396973
Kupo <jacobpmitchell@gmail.com>
2022-09-06 10:29:53 -0700

    hehe

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
 # Test pycode for data
-
+# input()
 import article
-from readers import get_readings_from_file
+import readers
+import tweets
+
+# better way to handle this, look into refactoring
+class genericDataStructure:
+    def __init__(self, type: str):
+        self.internalType = str
+        if str is "twitter":
+            self.struct = tweets.TweetCollection()
+        elif str is "web" or "article":
+            self.struct = article.ArticleCollection()
+        else:
+            self.struct = None
+
+    # check this behaves as intended
+    def validate(self):
+        return self.internalType is not None
+
+    def import(self, fileName: str):
+
+        if not self.validate():
+            return None
+
+        if self.internalType is "twitter":
+            self.struct = readers.read_tweet_csv_file(fileName)
+        else:
+            self.struct = readers.read_article_data(fileName)
+
+    def use(self):
+        return self.struct
+
+# god I love switch cases
+def switch(arg: str):
+    if arg is ""
 
 if __name__ == '__main__':
+    # outline
+    case = input("Which kind of file?:")
+
+    mainData = genericDataStructure(case)
+
+    fileName = input("Enter file name with suffix:")
+    mainData.import(fileName)
+
+    
